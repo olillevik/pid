d007a08547dae35dcebfeec0de6d94015e28d6c0
Ørjan Markhus Lillevik <olillevik@gmail.com>
2016-09-29 21:55:41 +0200

    generate random fnr

--- script.js
+++ script.js
@@ -1,34 +1,67 @@
 (function () {
-    'use strcit'
+    'use strict'
+
+    var fnr = () => {
+        const randomDate = randomDateBetween(new Date(1854, 1), new Date());
+        const dateISO = randomDate.toISOString();
+        const individsiffer = individsifferAsString(randomIndividsiffer(randomDate.getFullYear()));
+
+        const d1 = dateISO.substring(8, 9);
+        const d2 = dateISO.substring(9, 10);
+        const m1 = dateISO.substring(5, 6);
+        const m2 = dateISO.substring(6, 7);
+        const aa1 = dateISO.substring(2, 3);
+        const aa2 = dateISO.substring(3, 4);
+
+        const i1 = individsiffer.substring(0, 1);
+        const i2 = individsiffer.substring(1, 2);
+        const i3 = individsiffer.substring(2, 3);
+
+        const k1 = elevenGivesZero(11 - ((3 * d1 + 7 * d2 + 6 * m1 + 1 * m2 + 8 * aa1 + 9 * aa2 + 4 * i1 + 5 * i2 + 2 * i3) % 11));
+        const k2 = elevenGivesZero(11 - ((5 * d1 + 4 * d2 + 3 * m1 + 2 * m2 + 7 * aa1 + 6 * aa2 + 5 * i1 + 4 * i2 + 3 * i3 + 2 * k1) % 11));
+
+        const ret = "" + d1 + d2 + m1 + m2 + aa1 + aa2 + i1 + i2 + i3 + k1 + k2;
+
+        if (ret.length === 11)
+            return ret;
+        else
+            return fnr();
+    };
+
+    var elevenGivesZero = (number) => {
+        if (number === 11) {
+            return 0;
+        }
+        return number;
+    }
 
     var el = id => document.getElementById("" + id);
 
-    var fnr = () => {
-        // fødselsdato 2000-01-01
-        const d1 = 0;
-        const d2 = 1;
-        const m1 = 0;
-        const m2 = 1;
-        const aa1 = 2;
-        const aa2 = 0;
-        const aa3 = 0;
-        const aa4 = 0;
-        // individsiffer 500
-        const i1 = 5;
-        const i2 = 0;
-        const i3 = 0;
+    var randomDateBetween = (d1, d2) => new Date(d1.getTime() + Math.random() * (d2.getTime() - d1.getTime()));
 
-        const k1 = 11 - ((3 * d1 + 7 * d2 + 6 * m1 + 1 * m2 + 8 * aa1 + 9 * aa2 + 4 * i1 + 5 * i2 + 2 * i3) % 11);
+    var randomIndividsiffer = year => {
+        if (year >= 1854 && year <= 1899)
+            return getRandomInt(500, 749);
+        if (year >= 1900 && year <= 1999)
+            return getRandomInt(0, 499);
+        if (year >= 2000 && year <= 2039)
+            return getRandomInt(500, 999);
+    };
 
-        const k2 = 11 - ((5 * d1 + 4 * d2 + 3 * m1 + 2 * m2 + 7 * aa1 + 6 * aa2 + 5 * i1 + 4 * i2 + 3 * i3 + 2 * k1) % 11);
+    var getRandomInt = (min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
 
-        return "" + d1 + d2 + m1 + m2 + aa1 + aa2 + aa3 + aa4 + i1 + i2 + i3 + k1 + k2;
-
-        //Individsiffer
-        //000–499 omfatter personer født i perioden 1900–1999.
-        //500–749 omfatter personer født i perioden 1854–1899.
-        //500–999 omfatter personer født i perioden 2000–2039.
-        //900–999 omfatter personer født i perioden 1940–1999.
+    var individsifferAsString = individsiffer => {
+        if (individsiffer <= 9) {
+            return "00" + individsiffer;
+        } else if (individsiffer <= 99) {
+            return "0" + individsiffer;
+        } else if (individsiffer > 99) {
+            return "" + individsiffer;
+        }
     };
 
     el("fnr").innerHTML = fnr();
